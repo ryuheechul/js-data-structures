7ba8c60d929a01b41975f95c178fa5a986dca7d7
RyuHee chul <heechul@RyuHeeui-MacBook.local>
2015-07-21 21:10:11 +0900

    added sort code.

---
+++ sort/shell-sort.js
@@ -0,0 +1,16 @@
+load("CArray.js");
+CArray.prototype.shellSort = shellsort;
+CArray.prototype.gaps = [5,3,1];
+function shellsort() {
+   for (var g = 0; g < this.gaps.length; ++g) {
+      for (var i = this.gaps[g]; i < this.dataStore.length; ++i) {
+         var temp = this.dataStore[i];
+         for (var j = i; j >= this.gaps[g] &&
+                         this.dataStore[j-this.gaps[g]] > temp;
+              j -= this.gaps[g]) {
+            this.dataStore[j] = this.dataStore[j - this.gaps[g]];
+         }
+         this.dataStore[j] = temp;
+      }
+   }
+}
